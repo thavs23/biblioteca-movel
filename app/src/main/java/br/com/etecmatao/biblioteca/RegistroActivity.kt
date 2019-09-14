a8dc556df0b3c9cb86989b15e377e5a249d33651
thavs23 <thais.siaht23@gmail.com>
2019-09-13 22:01:31 -0300

    Aula 06-09-2019

---
+++ app/src/main/java/br/com/etecmatao/biblioteca/RegistroActivity.kt
@@ -0,0 +1,12 @@
+package br.com.etecmatao.biblioteca
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
